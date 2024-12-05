b24f53e6492b0cd0503eb3d3fe4bb7ff470b7132
michelepatella <patellamichele2002@gmail.com>
2024-12-05 15:58:44 +0100

    navigation using phone menu is now allowed.

--- src/js/Menu.js
+++ src/js/Menu.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import '../css/Menu.css';
 import { Anchor, Button, Drawer, Menu as AntMenu } from "antd";
-import { MenuOutlined } from '@ant-design/icons';
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import { motion, useAnimation } from "framer-motion";
-import { CloseOutlined } from '@ant-design/icons';
 
 /**
  * Definition of the sections of the web page
@@ -18,40 +17,22 @@ const sections = [
 ];
 
 /**
- * Definition of the menu items for phone menu
- * @type {[{label: string, key: string},{label: string, key: string},{children: [{label: string, key: string},{label: string, key: string}], label: string, key: string},{label: string, key: string},{label: string, key: string}]}
- */
-const menuItemsForPhone = [
-    { key: 'home', label: 'Home' },
-    { key: 'chi-siamo', label: 'Chi siamo',
-        children: [{ key: 'nostri-valori', title: 'I nostri valori' }] },
-    {
-        key: 'servizi',
-        label: 'Servizi', href: '#servizi',
-        children: [
-            { key: 'altri-servizi', label: 'Altri servizi'},
-            { key: 'auto-usate', label: 'Auto usate'},
-        ],
-    },
-    { key: 'dove-siamo', label: 'Dove siamo'},
-    { key: 'contatti', label: 'Contatti'},
-];
-
-/**
  * Method to animate the transition to a section
  * @param sectionId
  */
-const scrollToSection = (sectionId) => {
+const scrollToSection = (sectionId, isPhoneMenu, setVisible) => {
     const section = document.getElementById(sectionId);
-    console.log('Scrolling to section:', sectionId);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (isPhoneMenu) {
+        setVisible(false);
     }
 };
 
 /**
  * Generation of menu items for PC and tablet
- * @type {{onClick: function(): void, children: *|undefined, href: string, title: *, key: string}[]}
  */
 const menuItemsGenerationForPCAndTablet = sections.map((section, index) => ({
     key: `section-${index + 1}`,
@@ -62,42 +43,11 @@ const menuItemsGenerationForPCAndTablet = sections.map((section, index) => ({
             key: `section-${index + 1}-child-${childIndex + 1}`,
             href: `#${child.id}`,
             title: child.title,
-            onClick: () => scrollToSection(child.id),
+            onClick: () => scrollToSection(child.id, false),
         }))
         : undefined,
-    onClick: () => scrollToSection(section.id)
+    onClick: () => scrollToSection(section.id, false)
 }));
-
-/**
- * Generation of menu items for phone
- * @param data
- * @returns {*}
- */
-const menuItemsGenerationForPhone = (data) => {
-    return data.map(item => {
-
-        if (item.children) {
-            return (
-                <AntMenu.SubMenu
-                    key={item.key}
-                    title={item.label}
-                >
-                    {menuItemsGenerationForPhone(item.children)}
-                </AntMenu.SubMenu>
-            );
-        }
-
-        return (
-            <AntMenu.Item
-                key={item.key}
-                onClick={() => scrollToSection(item.key)}
-            >
-                {item.label}
-            </AntMenu.Item>
-        );
-    });
-};
-
 
 /**
  * This component contains the menu of the web page
@@ -105,6 +55,8 @@ const menuItemsGenerationForPhone = (data) => {
  * @constructor
  */
 const Menu = () => {
+    const [visible, setVisible] = useState(false);
+
     /**
      * Animation for the menu of PC and tablet
      * @type {AnimationControls}
@@ -114,9 +66,11 @@ const Menu = () => {
         controls.start({ opacity: 1, x: 0, transition: { duration: 0.8 } });
     }, [controls]);
 
-    // Animation for the menu items in mobile
+    /**
+     * Animation for the menu item for phone
+     * @type {AnimationControls}
+     */
     const menuItemControls = useAnimation();
-
     useEffect(() => {
         menuItemControls.start({
             opacity: 1,
@@ -129,11 +83,27 @@ const Menu = () => {
         });
     }, [menuItemControls]);
 
-    const [visible, setVisible] = useState(false);
-
     const toggleDrawer = () => {
         setVisible(!visible);
     };
+
+    /**
+     * Generate menu items for phone and pass setVisible
+     */
+    const menuItemsGenerationForPhone = sections.map((section, index) => ({
+        key: `section-${index + 1}`,
+        href: `#${section.id}`,
+        label: section.title,
+        children: section.children
+            ? section.children.map((child, childIndex) => ({
+                key: `section-${index + 1}-child-${childIndex + 1}`,
+                href: `#${child.id}`,
+                label: child.title,
+                onClick: () => scrollToSection(child.id, true, setVisible),
+            }))
+            : undefined,
+        onClick: () => scrollToSection(section.id, true, setVisible)
+    }));
 
     return (
         <>
@@ -172,15 +142,14 @@ const Menu = () => {
                     width={250}
                     closeIcon={<CloseOutlined style={{ fontSize: '24px', color: 'black' }} />}
                 >
-                    {/* Motion Div applied to animate menu items on the mobile */}
                     <motion.div
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ duration: 0.5 }}
                     >
                         <AntMenu
-                            mode="vertical"
-                            items={menuItemsForPhone}
+                            mode="inline" // Usato per il comportamento inline del sottomenu
+                            items={menuItemsGenerationForPhone}
                         />
                     </motion.div>
                 </Drawer>
