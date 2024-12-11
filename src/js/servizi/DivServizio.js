bc527f3c18898615b805aa423d47bcce540b07b5
michelepatella <patellamichele2002@gmail.com>
2024-12-11 11:24:49 +0100

    altri servizi section completed.

--- src/js/servizi/DivServizio.js
+++ src/js/servizi/DivServizio.js
@@ -1,5 +1,6 @@
 import '../../css/servizi/DivServizio.css';
 import React, {useEffect, useRef} from "react";
+import {Flex} from "antd";
 
 /**
  * This component define the structure of each division inserted into the Altri servizi section
@@ -12,6 +13,7 @@ function DivServizio({ title, description, icon}){
 
     const titleRef = useRef(null);
     const subtitleRef = useRef(null);
+    const iconRef = useRef(null);
 
     /**
      * Method to keep track if the section Altri servizi is visible
@@ -35,18 +37,24 @@ function DivServizio({ title, description, icon}){
 
         if (titleRef.current) observer.observe(titleRef.current);
         if (subtitleRef.current) observer.observe(subtitleRef.current);
+        if (iconRef.current) observer.observe(iconRef.current);
 
         return () => {
             if (titleRef.current) observer.unobserve(titleRef.current);
             if (subtitleRef.current) observer.unobserve(subtitleRef.current);
+            if (iconRef.current) observer.unobserve(iconRef.current);
         };
     }, []);
 
     return (
         <div className="divServizio">
-            {/*<img className="cardIcon" src={icon}/>*/}
-            <p className="title-servizio" ref={titleRef}>{title}</p>
-            <p className="description-servizio"ref={subtitleRef}>{description}</p>
+            <Flex gap="small" align="start" vertical className="title-and-icon-container">
+                <Flex className="flex-division">
+                    <p className="title-servizio" ref={titleRef}>{title}</p>
+                    <img className="servizio-icon" src={icon} ref={iconRef} />
+                </Flex>
+            </Flex>
+            <p className="description-servizio" ref={subtitleRef}>{description}</p>
         </div>
     );
 }
