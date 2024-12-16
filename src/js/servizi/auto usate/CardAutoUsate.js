d207383447bf2b4f6238b32a2addf2f3adc53cc5
michelepatella <patellamichele2002@gmail.com>
2024-12-16 15:34:13 +0100

    auto usate section almost completed

--- src/js/servizi/auto usate/CardAutoUsate.js
+++ src/js/servizi/auto usate/CardAutoUsate.js
@@ -1,7 +1,8 @@
 import {Col, Statistic} from "antd";
-import React from "react";
+import React, {useState} from "react";
 import '../../../css/servizi/auto usate/CardAutoUsate.css';
 import CountUp from 'react-countup';
+import {useInView} from 'react-intersection-observer';
 
 /**
  * This component represents the informative card of auto usate section
@@ -10,13 +11,33 @@ import CountUp from 'react-countup';
  * @constructor
  */
 function CardAutoUsate({ description }) {
+    const [hasStarted, setHasStarted] = useState(false);
 
-    const formatter = (value) => <CountUp end={value}/>;
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
+
+
+    React.useEffect(() => {
+        if (inView && !hasStarted) {
+            setHasStarted(true);
+        }
+    }, [inView, hasStarted]);
+
+    const formatter = (value) => hasStarted ? <CountUp end={value} /> : <span>0</span>;
 
     return (
         <div>
-            <Col className="statistic-division">
-                <Statistic title={description} value={100} suffix="%" className="custom-statistic" formatter={formatter}/>
+            <Col className="statistic-division" ref={ref}>
+                <Statistic
+                    title={description}
+                    style={{fontWeight: "bold"}}
+                    value={hasStarted ? 100 : 0}
+                    suffix="%"
+                    className="custom-statistic"
+                    formatter={formatter}
+                />
             </Col>
         </div>
     );
