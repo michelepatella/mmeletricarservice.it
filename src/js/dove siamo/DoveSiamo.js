0dcf54ef0a37f9eb3711560577b9499c70eb9cef
michelepatella <patellamichele2002@gmail.com>
2025-02-08 17:13:45 +0100

    cookie banner implementation

--- src/js/dove siamo/DoveSiamo.js
+++ src/js/dove siamo/DoveSiamo.js
@@ -1,7 +1,9 @@
 import '../../css/dove siamo/DoveSiamo.css';
 import {Row} from "antd";
 import {DOVE_SIAMO_DIVISIONS, DOVE_SIAMO_TITLE} from "../utility/constants";
+import CookieConsent from "react-cookie-consent";
 import DivDoveSiamo from "./DivDoveSiamo";
+import {useState} from "react";
 
 /**
  * This component contains the dove siamo section
@@ -9,6 +11,22 @@ import DivDoveSiamo from "./DivDoveSiamo";
  * @constructor
  */
 function DoveSiamo() {
+
+    const [cookiesAccepted, setCookiesAccepted] = useState(false);
+
+    /**
+     * Method to handle the accepting of the cookie
+     */
+    const handleAcceptCookies = () => {
+        setCookiesAccepted(true);
+    };
+
+    /**
+     * Method to handle the declining of the cookie
+     */
+    const handleDeclineCookies = () => {
+        setCookiesAccepted(false);
+    };
 
     return (
         <div id="dove-siamo" className="dove-siamo-container">
@@ -26,16 +44,57 @@ function DoveSiamo() {
                         />
                     ))}
 
-                    <div className="google-map-container">
+                    {/*Google Maps (shown only in case of cookie accepting)*/}
+                    {cookiesAccepted && (
+                        <div className="google-map-container">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.143408468909!2d16.4256612!3d40.8248136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13387b9989c61785%3A0xaa80de53bd394020!2sMM%20Eletricar%20Service%20di%20Vacca%20%26%20Scalese%20snc%20%E2%80%A2%20Meccatronica%20%E2%80%A2%20Elettrauto!5e0!3m2!1sit!2sit!4v1735907615314!5m2!1sit!2sit"
+                                allowFullScreen="" loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade" title="google-maps"
+                                className="google-maps-iframe">
+                            </iframe>
+                        </div>
+                    )}
 
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.143408468909!2d16.4256612!3d40.8248136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13387b9989c61785%3A0xaa80de53bd394020!2sMM%20Eletricar%20Service%20di%20Vacca%20%26%20Scalese%20snc%20%E2%80%A2%20Meccatronica%20%E2%80%A2%20Elettrauto!5e0!3m2!1sit!2sit!4v1735907615314!5m2!1sit!2sit"
-                            allowFullScreen="" loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade" title="google-maps" className="google-maps-iframe">
-                        </iframe>
-                    </div>
                 </Row>
             </Row>
+
+            {/*Banner for consenting/declining cookie*/}
+            <CookieConsent
+                location="bottom"
+                buttonText="Accetta"
+                declineButtonText="Rifiuta"
+                cookieName="cookieConsent"
+                style={{
+                    background: "#141414",
+                    color: "white",
+                    fontFamily: 'Poppins',
+                    padding: "10px",
+                    fontSize: "14px",
+                }}
+                buttonStyle={{
+                    background: "#F8DE4D",
+                    color: "black",
+                    fontFamily: 'Poppins',
+                    fontSize: "14px",
+                    padding: "10px 20px",
+                    borderRadius: "10px"
+                }}
+                declineButtonStyle={{
+                    background: "#FF6347",
+                    color: "#fff",
+                    fontFamily: 'Poppins',
+                    fontSize: "14px",
+                    padding: "10px 20px",
+                    borderRadius: "10px"
+                }}
+                expires={0}
+                onAccept={handleAcceptCookies}
+                onDecline={handleDeclineCookies}
+            >
+                Questo sito utilizza i cookie per offrirti una migliore esperienza di navigazione.
+                Cliccando su "Accetta", acconsenti all'uso dei cookie.
+            </CookieConsent>
 
         </div>
     );
