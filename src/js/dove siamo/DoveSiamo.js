09f8f80e67c722c6cbff032dc6fd6b526600d324
michelepatella <patellamichele2002@gmail.com>
2025-02-09 21:42:58 +0100

    google maps added again

--- src/js/dove siamo/DoveSiamo.js
+++ src/js/dove siamo/DoveSiamo.js
@@ -32,12 +32,12 @@ function DoveSiamo() {
 
                     {/*Google Maps (shown only in case of cookie accepting)*/}
                         <div className="google-map-container">
-                            {/*<iframe
+                            <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.143408468909!2d16.4256612!3d40.8248136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13387b9989c61785%3A0xaa80de53bd394020!2sMM%20Eletricar%20Service%20di%20Vacca%20%26%20Scalese%20snc%20%E2%80%A2%20Meccatronica%20%E2%80%A2%20Elettrauto!5e0!3m2!1sit!2sit!4v1735907615314!5m2!1sit!2sit"
                                 allowFullScreen="" loading="lazy"
                                 referrerPolicy="no-referrer-when-downgrade" title="google-maps"
                                 className="google-maps-iframe">
-                            </iframe>*/}
+                            </iframe>
                         </div>
                 </Row>
             </Row>
