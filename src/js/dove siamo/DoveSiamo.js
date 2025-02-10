a1762fa7c22a1849aec73b3b4a236137197f6351
michelepatella <patellamichele2002@gmail.com>
2025-02-10 12:10:11 +0100

    font uploading changed, new cookies button style and updates concerns cookies banner

--- src/js/dove siamo/DoveSiamo.js
+++ src/js/dove siamo/DoveSiamo.js
@@ -1,5 +1,5 @@
 import '../../css/dove siamo/DoveSiamo.css';
-import {Button, Row} from "antd";
+import {Button, Row, Switch} from "antd";
 import {DOVE_SIAMO_DIVISIONS, DOVE_SIAMO_TITLE} from "../utility/constants";
 import CookieConsent from "react-cookie-consent";
 import DivDoveSiamo from "./DivDoveSiamo";
@@ -13,8 +13,75 @@ import React, {useEffect, useState} from "react";
  */
 function DoveSiamo() {
 
+    const [cookiesAccepted, setCookiesAccepted] = useState(false);
+    const [isCookiesBannerVisible, setIsCookiesBannerVisible] = useState(false);
+
+    const [cookies, setCookies] = useState({
+        necessary: false,
+        preferences: false,
+        analytics: false,
+        advertising: false
+    });
+
+    const handleChange = (type) => {
+        setCookies(prevState => ({
+            ...prevState,
+            [type]: !prevState[type]
+        }));
+    };
+
+    /**
+     * Method to check if the cookies have already been accepted or not
+     */
+    useEffect(() => {
+
+        const cookieConsent = Cookies.get('cookieConsent');
+
+        if (cookieConsent === 'true') {
+            setCookiesAccepted(true);
+        }
+    }, []);
+
+
+    /**
+     * Method to handle the accepting of the cookie
+     */
+    const handleAcceptCookies = () => {
+        setCookiesAccepted(true);
+        setIsCookiesBannerVisible(false);
+    };
+
+    /**
+     * Method to handle the declining of the cookie
+     */
+    const handleDeclineCookies = () => {
+        setCookiesAccepted(false);
+        setIsCookiesBannerVisible(false);
+    };
+
+    /**
+     * Method to handle clicking on the cookie button
+     */
+    const handleCookieButtonClick = () => {
+        setCookiesAccepted(false);
+        setIsCookiesBannerVisible(true);
+
+        //remove all cookies
+        const allCookies = Cookies.get();
+
+        for (let cookieName in allCookies) {
+            if (allCookies.hasOwnProperty(cookieName)) {
+                Cookies.remove(cookieName);
+            }
+        }
+
+    };
+
     return (
         <div id="dove-siamo" className="dove-siamo-container">
+
+            {/*Cookies button */}
+            <Button className="cookie-button" onClick={handleCookieButtonClick} />
 
             {/* Title */}
             <h2 className="dove-siamo-title">{DOVE_SIAMO_TITLE}</h2>
@@ -31,6 +98,7 @@ function DoveSiamo() {
                     ))}
 
                     {/*Google Maps (shown only in case of cookie accepting)*/}
+                    {cookiesAccepted && (
                         <div className="google-map-container">
                             <iframe
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.143408468909!2d16.4256612!3d40.8248136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13387b9989c61785%3A0xaa80de53bd394020!2sMM%20Eletricar%20Service%20di%20Vacca%20%26%20Scalese%20snc%20%E2%80%A2%20Meccatronica%20%E2%80%A2%20Elettrauto!5e0!3m2!1sit!2sit!4v1735907615314!5m2!1sit!2sit"
@@ -39,9 +107,81 @@ function DoveSiamo() {
                                 className="google-maps-iframe">
                             </iframe>
                         </div>
+                    )}
                 </Row>
             </Row>
 
+            {/*Banner for consenting/declining cookie*/}
+            { isCookiesBannerVisible &&
+                <CookieConsent
+                    location="bottom"
+                    buttonText="Accetta tutti"
+                    declineButtonText="Rifiuta tutti"
+                    cookieName="cookieConsent"
+                    style={{
+                        zIndex: 9999999,
+                        background: "#141414",
+                        color: "white",
+                        fontFamily: 'Poppins',
+                        padding: "10px",
+                        fontSize: "14px",
+                    }}
+                    buttonStyle={{
+                        background: "#F8DE4D",
+                        color: "black",
+                        fontFamily: 'Poppins',
+                        fontSize: "14px",
+                        padding: "10px 20px",
+                        borderRadius: "10px"
+                    }}
+                    declineButtonStyle={{
+                        background: "#3C3C3C",
+                        color: "white",
+                        fontFamily: 'Poppins',
+                        fontSize: "14px",
+                        padding: "10px 20px",
+                        borderRadius: "10px"
+                    }}
+                    expires={30}
+                    enableDeclineButton={true}
+                    visible={true}
+                    onAccept={handleAcceptCookies}
+                    onDecline={handleDeclineCookies}
+                >
+                    Questo sito utilizza i <strong>cookie</strong>.
+                    Cliccando su "Accetta", acconsenti all'uso dei cookie.
+                    Cliccando su "Rifiuta", rifiuti l'uso dei cookie.
+                    Per maggiori informazioni consulta la nostra <a href="#privacy-policy" style={{
+                    color: 'white',
+                    textDecoration: 'underline',
+                    fontWeight: 'bold'
+                }}>
+                    Privacy Policy
+                </a>
+                    🍪
+
+                    {/*Switch for technique and security cookies*/}
+                    <div style={{display: 'flex', alignItems: 'center', marginTop: '10px'}}>
+                        <Switch
+                            checked={true}
+                            disabled
+                            style={{backgroundColor: '#F8DE4D'}}
+                        />
+                        <p style={{marginLeft: '10px'}}>Cookies tecnici e di sicurezza</p>
+                    </div>
+
+                    {/*Switch for third-party cookies*/}
+                    <div style={{display: 'flex', alignItems: 'center'}}>
+                        <Switch
+                            checked={cookies.preferences}
+                            onChange={() => handleChange('preferences')}
+                            style={{backgroundColor: cookies.preferences ? '#F8DE4D' : '#3C3C3C'}}
+                        />
+                        <p style={{marginLeft: '10px'}}>Cookies di terze parti</p>
+                    </div>
+
+                </CookieConsent>
+            }
         </div>
     );
 }
