2b9d639ac85ccc4983211d520a2a26ef06d2a01e
michelepatella <patellamichele2002@gmail.com>
2025-02-23 16:01:33 +0100

    phone number and icon updated

--- src/js/utility/constants.js
+++ src/js/utility/constants.js
@@ -5,7 +5,7 @@ export const WHATSAPP_LINK = 'https://wa.me/3908650830725';
 export const EMAIL = 'mmeletricarservice@libero.it';
 export const EMAIL_PEC = 'mmeletricarservice@pec.buffetti.it'
 export const FACEBOOK = ""
-export const PHONE = '080 3267837'
+export const PHONE = '0803267837'
 export const P_IVA = '08650830725'
 export const CONTATTI = [
     {value: PHONE, icon: '/icons/phoneIconBlack.svg'},
